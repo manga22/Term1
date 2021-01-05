9b435961b8df8b32210bab444da4afaadf13eaca
Manogna <manogna63@gmail.com>
2021-01-05 19:49:03 +0530

    Assignment4 draft

---
+++ Assignment_4/manogna_sreenivas/src/Wrapper.py
@@ -0,0 +1,189 @@
+# Wrapper file to test all functions
+import time
+from pathlib import Path
+
+import cv2
+import numpy as np
+from skimage import filters
+from skimage.io import imread
+from scipy.io import loadmat
+from skimage.transform import resize
+from matplotlib import pyplot as plt
+from matplotlib.colors import NoNorm
+
+from manogna_sreenivas.AllFunctions import decimate, image_deblur, image_denoise, \
+    wiener_filter, inverse_filter, cls_filter, bilateral_filter, edge_detect
+
+barbara_path = Path('../Data/images/barbara.tif')
+blur_kernel_path = Path('../Data/images/BlurKernel.mat')
+blur_high_path = Path('../Data/images/Blurred-HighNoise.png')
+blur_low_path = Path('../Data/images/Blurred-LowNoise.png')
+blur_med_path = Path('../Data/images/Blurred-MedNoise.png')
+noisybook1_path = Path('../Data/images/noisy-book1.png')
+noisybook2_path = Path('../Data/images/noisy-book2.png')
+origbook_path = Path('../Data/images/Original-book.png')
+characters_path = Path('../Data/images/characters.tif')
+
+
+def problem1():
+    low_noise = imread(blur_low_path)
+    med_noise = imread(blur_med_path)
+    high_noise = imread(blur_high_path)
+    blur_kernel = loadmat(blur_kernel_path)['h']
+
+    low_weiner_deblur = wiener_filter(low_noise, blur_kernel, noise_var=1)
+    med_weiner_deblur = wiener_filter(med_noise, blur_kernel, noise_var=5)
+    high_weiner_deblur = wiener_filter(high_noise, blur_kernel, noise_var=10)
+
+    plt.subplot(121, title='Low noise')
+    plt.imshow(low_noise, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Weiner filtered')
+    plt.imshow(low_weiner_deblur, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    plt.subplot(121, title='Med noise')
+    plt.imshow(med_noise, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Weiner filtered')
+    plt.imshow(med_weiner_deblur, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    plt.subplot(121, title='High noise')
+    plt.imshow(high_noise, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Weiner filtered')
+    plt.imshow(high_weiner_deblur, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    low_inverse_deblur = inverse_filter(low_noise, blur_kernel)
+    med_inverse_deblur = inverse_filter(med_noise, blur_kernel)
+    high_inverse_deblur = inverse_filter(high_noise, blur_kernel)
+
+    plt.subplot(121, title='Low noise')
+    plt.imshow(low_noise, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Inverse filtered')
+    plt.imshow(low_inverse_deblur, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    plt.subplot(121, title='Med noise')
+    plt.imshow(med_noise, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Inverse filtered')
+    plt.imshow(med_inverse_deblur, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    plt.subplot(121, title='High noise')
+    plt.imshow(high_noise, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Inverse filtered')
+    plt.imshow(high_inverse_deblur, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    low_cls_deblur = cls_filter(low_noise, blur_kernel)
+    med_cls_deblur = cls_filter(med_noise, blur_kernel)
+    high_cls_deblur = cls_filter(high_noise, blur_kernel)
+
+    plt.subplot(121, title='Low noise')
+    plt.imshow(low_noise, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='CLS filtered')
+    plt.imshow(low_cls_deblur, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    plt.subplot(121, title='Med noise')
+    plt.imshow(med_noise, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='CLS filtered')
+    plt.imshow(med_cls_deblur, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    plt.subplot(121, title='High noise')
+    plt.imshow(high_noise, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='CLS filtered')
+    plt.imshow(high_cls_deblur, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    return
+
+
+def problem2():
+    noisybook1 = imread(noisybook1_path)
+    noisybook2 = imread(noisybook2_path)
+    gaussian_filt = image_denoise(noisybook1, type='Gaussian', param=5)  # param = sigma for Gaussian kernel
+    median_filt = image_denoise(noisybook1, type='Median', param=5)  # param = size of neighbourhood
+    bilateral_filt = bilateral_filter(noisybook2,
+                                      param=[3, 0.3, 7])  # param = [sigma_gaussian, signma_intensity_diff, kernel_size]
+    gaussian2_filt = image_denoise(noisybook2, type='Gaussian', param=3)
+
+    plt.subplot(121, title='Noisy book1')
+    plt.imshow(noisybook1, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Gaussian filtered')
+    plt.imshow(gaussian_filt, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    plt.subplot(121, title='Noisy book1')
+    plt.imshow(noisybook1, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Median filtered')
+    plt.imshow(median_filt, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    plt.subplot(121, title='Noisy book2')
+    plt.imshow(noisybook2, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Bilateral filtered')
+    plt.imshow(bilateral_filt, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    plt.subplot(121, title='Noisy book2')
+    plt.imshow(noisybook2, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(122, title='Gaussian filtered')
+    plt.imshow(gaussian2_filt, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    return
+
+
+def problem3():
+    barbara = imread(barbara_path)
+    deci_image = decimate(barbara, d=2)
+    deci_lpf_image = decimate(barbara, d=2, filter='Gaussian', D0=100)
+    sk_resize_image = resize(barbara, (barbara.shape[0] // 2, barbara.shape[1] // 2))
+
+    cv2.imshow('Image before resize', barbara)
+    cv2.imshow('Decimation by 2', deci_image)
+    cv2.imshow('Gaussian LPF and Decimation by 2', deci_lpf_image)
+    cv2.imshow('Resize using lib', sk_resize_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return
+
+
+def problem4():
+    book = imread(origbook_path)
+    sobel_out = edge_detect(book, thresh=70, type='Sobel', plot_grad=True)
+    prewitt_out = edge_detect(book, thresh=70, type='Prewitt')
+    plt.subplot(131, title='Input image')
+    plt.imshow(book, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(132, title='Sobel filter')
+    plt.imshow(sobel_out, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(133, title='Prewitt filter')
+    plt.imshow(prewitt_out, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+
+    characters = imread(characters_path)
+    sobel_out = edge_detect(characters, thresh=25, type='Sobel', plot_grad=True)
+    prewitt_out = edge_detect(characters, thresh=100, type='Prewitt')
+    plt.subplot(131, title='Input image')
+    plt.imshow(characters, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(132, title='Sobel filter(Thr=25)')
+    plt.imshow(sobel_out, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.subplot(133, title='Prewitt filter(Thr=100)')
+    plt.imshow(prewitt_out, cmap='gray', vmin=0, vmax=255, interpolation=None)
+    plt.show()
+    return
+
+
+def main():
+    problem1()
+    problem2()
+    problem3()
+    problem4()
+
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ Assignment_4/manogna_sreenivas/src/manogna_sreenivas/AllFunctions.py
@@ -0,0 +1,160 @@
+# This file should contain all the functions required by Wrapper.py
+
+import numpy as np
+import skimage
+from numpy.fft import fft2, ifft2
+from skimage.filters import gaussian
+from skimage.filters.rank import median
+from skimage.morphology import square
+import matplotlib.pyplot as plt
+
+from manogna_sreenivas.helper import get_padded_image, get_distance_matrix, freq_filter, convolve
+
+
+def decimate(image: np.ndarray, d=2, filter=None, D0=None) -> np.ndarray:
+    im_size = image.shape
+    if filter == 'Gaussian':
+        D = get_distance_matrix(im_size)
+        H = np.zeros(im_size)
+        H = np.exp(-1 * (D ** 2) / (2 * (D0 ** 2)))
+
+        # Get image after filtering in frequency domain
+        im_filtered = freq_filter(image, H)
+        image = np.clip(im_filtered, 0, 255)
+        image = np.array(image, dtype=np.uint8)
+
+    deci_image = np.zeros((int(im_size[0] / 2), int(im_size[1] / 2)))
+    y, x = np.indices(deci_image.shape) * d
+    deci_image = image[y, x]
+    return deci_image
+
+
+def wiener_filter(image: np.ndarray, h: np.ndarray, noise_var=5) -> np.ndarray:
+    image = skimage.img_as_float(image)
+    M, N = image.shape
+    H = fft2(h, s=image.shape)                  # FFT of given blur filter
+    nsr = 0.01
+    D = np.conj(H) / (np.abs(H) ** 2 + nsr)     # Weiner filter
+    f_image = fft2(image)                       # get fft of image
+    im_filtered = ifft2(f_image * D)            # Filter image in frequency domain
+    im_filtered = np.abs(im_filtered)
+    deblur_im = np.uint8(np.clip(im_filtered, 0, 1) * 255)
+    return deblur_im
+
+
+def inverse_filter(image: np.ndarray, h: np.ndarray) -> np.ndarray:
+    image = skimage.img_as_float(image)
+    M, N = image.shape
+    H = fft2(h, s=image.shape)                  # FFT of given blur filter
+    D = 1 / H                                   # Wiener filter
+    f_image = fft2(image)                       # get fft of image
+    im_filtered = ifft2(f_image * D)            # Filter image in frequency domain
+    im_filtered = np.abs(im_filtered)
+    deblur_im = (im_filtered - np.min(im_filtered)) / (np.max(im_filtered) - np.min(im_filtered)) * 255
+    return deblur_im
+
+
+def cls_filter(image: np.ndarray, h: np.ndarray, gamma=10**(-2)) -> np.ndarray:
+    image = skimage.img_as_float(image)
+    M, N = image.shape
+    H = fft2(h, s=image.shape)                  # FFT of given blur filter
+
+    p = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])     # Get CLS filter
+    P = fft2(p, s=image.shape)
+    D = np.conj(H) / (np.abs(H) ** 2 + gamma * np.abs(P) ** 2)
+
+    f_image = fft2(image)                       # get fft of image
+    im_filtered = ifft2(f_image * D)            # Filter image in frequency domain
+    im_filtered = np.abs(im_filtered)
+    deblur_im = np.uint8(np.clip(im_filtered, 0, 1) * 255)
+    return deblur_im
+
+
+def image_deblur(image: np.ndarray, h: np.ndarray, noise_var=5, type='Wiener') -> np.ndarray:
+    norm = True
+    M, N = image.shape
+    H = fft2(h, s=image.shape)
+    image = skimage.img_as_float(image)
+    if type == 'Wiener':
+        u, v = np.indices(image.shape)
+        S_w = noise_var ** 2
+        nsr = S_w / (S_w * 10 * np.exp(-(np.abs(u - M // 2) - np.abs(v - N // 2)) / 5))
+        # nsr = ifftshift(nsr)
+        # print(nsr)
+        nsr = 0.01
+        D = np.conj(H) / (np.abs(H) ** 2 + nsr)
+    if type == 'Inverse':
+        nsr = 0
+        D = 1 / H
+        norm = False
+    if type == 'CLS':
+        p = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+        P = fft2(p, s=image.shape)
+        gamma = 10 ** (-2)
+        D = np.conj(H) / (np.abs(H) ** 2 + gamma * np.abs(P) ** 2)
+
+    f_image = fft2(image)  # get fft of image
+    im_filtered = ifft2(f_image * D)
+    im_filtered = np.abs(im_filtered)
+
+    deblur_im = (im_filtered - np.min(im_filtered)) / (np.max(im_filtered) - np.min(im_filtered)) * 255
+    # plt.imshow(deblur_im, cmap = 'gray')
+    # plt.show()
+
+    if norm:
+        print(type)
+        deblur_im = np.uint8(np.clip(im_filtered, 0, 1) * 255)
+    return deblur_im
+
+
+def image_denoise(image: np.ndarray, type='Gaussian', param=1) -> np.ndarray:
+    if type == 'Gaussian':
+        denoised_img = gaussian(image, sigma=param)
+        denoised_img = np.uint8(denoised_img * 255)
+    if type == 'Median':
+        denoised_img = median(image, square(param))
+    return denoised_img
+
+
+def bilateral_filter(image: np.ndarray, param: list) -> np.ndarray:
+    image = skimage.img_as_float(image)
+    sigma_g, sigma_h, k_size = param
+
+    k_h = k_w = k_size
+    h, w = image.shape
+    d = get_distance_matrix((k_h, k_w))
+    G = np.exp(-d / (2 * sigma_g ** 2))  # spatial smoothing Gaussian filter
+    padded_image = get_padded_image(image, (k_h, k_w))
+    conv_image = np.zeros(image.shape, dtype=float)
+
+    K = 0
+    for i in range(k_h):
+        for j in range(k_w):
+            I_diff = padded_image[i:i + h, j:j + w] - image
+            H = np.exp(-I_diff ** 2 / (2 * sigma_h ** 2))  # Gaussian using Luminance distance
+            conv_image += padded_image[i:i + h, j:j + w] * G[i, j] * H
+            K += G[i, j] * H  # Collect weights to normalize
+
+    filtered_im = np.uint8(conv_image / K * 255)
+    return filtered_im
+
+
+def edge_detect(image: np.ndarray, thresh=70, type='Sobel', plot_grad=False) -> np.ndarray:
+    image = gaussian(image, sigma=3)
+    if type == 'Sobel':
+        Gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+        Gy = Gx.T
+    elif type == 'Prewitt':
+        Gx = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+        Gy = Gx.T
+    I_x = convolve(image, Gx)  # Horizontal gradients
+    I_y = convolve(image, Gy)  # Vertical gradients
+    grad = np.sqrt(I_x ** 2 + I_y ** 2)
+    grad = (grad - np.min(grad)) / (np.max(grad) - np.min(grad)) * 255
+    if plot_grad:
+        plt.imshow(grad, cmap='gray')
+        plt.title('Gradient image')
+        plt.show()
+    binary = grad > thresh
+    binary = np.uint8(binary * 255)
+    return binary
